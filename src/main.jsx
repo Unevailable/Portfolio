de01c0dd83af0c608dd446b23d6218fcdb7267d6
Al Amin <al.amin003@hotmail.com>
2024-02-12 18:56:44 -0500

    added updated code to work with react-router-dom.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
