56dca511653ae66b770b155eeaca2dc1580f4c5e
Al Amin <al.amin003@hotmail.com>
2024-03-03 00:11:53 -0500

    added a fix.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,47 @@
 import React from "react";
 import "./Contact.css";
 
-export default function Contact() {
+const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Additional logic for form submission if needed
+  };
 
-return <div>
-  <form 
-  name="contact info" 
-  method="post"
-  data-netlify="true"
-  onSubmit="submit"
->
-  <input type="hidden" name="form-name" value="contact info"/>
+  return (
+    <div>
+      <form
+        name="contact info"
+        method="post"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact info" />
 
-  <div>
-    <label> Name <br />
-        <input type="text" name="name"/>
-    </label>
-  </div>
+        <div>
+          <label>
+            Name <br />
+            <input type="text" name="name" />
+          </label>
+        </div>
 
-  <div>
-    <label htmlFor="email"> Email </label> <br />
-        <input id="email" type="email" name="email"/>
-  </div>
+        <div>
+          <label htmlFor="email">
+            Email <br />
+            <input id="email" type="email" name="email" />
+          </label>
+        </div>
 
-  <div>
-    <label> Message <br />
-        <textarea name="message"></textarea>
-    </label>
-  </div>
+        <div>
+          <label>
+            Message <br />
+            <textarea name="message"></textarea>
+          </label>
+        </div>
 
-  <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-</form>
-</div>
-
-}
+export default Contact;
