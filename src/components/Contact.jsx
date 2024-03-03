c7da223cd156177afdc3b1f623007bc2b0d601ce
Al Amin <al.amin003@hotmail.com>
2024-03-03 00:31:31 -0500

    test.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,35 +3,55 @@ import "./Contact.css";
 
 export default function Contact() {
 
-return <div>
-  <form 
-  name="contact info" 
-  method="post"
-  data-netlify="true"
-  onSubmit="submit"
->
-  <input type="hidden" name="form-name" value="contact info"/>
+  const handleSubmit = (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
-  <div>
-    <label> Name <br />
-        <input type="text" name="name"/>
-    </label>
-  </div>
+    // You can add additional logic here if needed
 
-  <div>
-    <label htmlFor="email"> Email </label> <br />
-        <input id="email" type="email" name="email"/>
-  </div>
+    // Submit the form using Netlify Forms
+    const form = event.target;
+    fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then(() => alert("Form submitted successfully"))
+      .catch((error) => alert(error));
+  };
 
-  <div>
-    <label> Message <br />
-        <textarea name="message"></textarea>
-    </label>
-  </div>
+  return (
+    <div>
+      <form
+        name="contact info"
+        method="post"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact info" />
 
-  <button type="submit">Submit</button>
+        <div>
+          <label>
+            Name <br />
+            <input type="text" name="name" />
+          </label>
+        </div>
 
-</form>
-</div>
+        <div>
+          <label htmlFor="email">
+            Email <br />
+            <input id="email" type="email" name="email" />
+          </label>
+        </div>
 
+        <div>
+          <label>
+            Message <br />
+            <textarea name="message"></textarea>
+          </label>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
